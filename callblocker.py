884ff463c33ec939cd2662828ba8ccf3039ee29f
bufemc <none>
2020-06-21 12:45:38 +0200

    Scoring outsourced, caller naming improved

--- callblocker.py
+++ callblocker.py
@@ -1,5 +1,6 @@
 import csv
 import logging
+from datetime import datetime
 
 import requests
 from callmonitor import CallMonitor, CallMonitorType, CallMonitorLine, CallMonitorLog
@@ -27,10 +28,48 @@ class CallBlockerLog(Log):
             f.write(line + "\n")
 
 
+class CallerInfo:
+    """ Retrieve details for a phone number. Currently scoring via Tellows. Could get score from a dict (caching). """
+
+    def __init__(self, number):
+        self.number = number
+        self.get_tellows_score()
+
+    def get_tellows_score(self):
+        """ Do scoring for a phone number via Tellows.
+        https://blog.tellows.de/2011/07/tellows-api-fur-die-integration-in-eigene-programme/ """
+        url = f'http://www.tellows.de/basic/num/{self.number}?json=1&partner=test&apikey=test123'
+        # ToDo: failed requests, blocked, json malformed..
+        req = requests.get(url)
+        obj = req.json()['tellows']
+
+        self.score = int(obj['score'])
+        self.comment_count = int(obj['comments'])
+        self.location = obj['location']
+        self.searches = obj['searches']
+
+        # Build smarter name, e.g. from callerTypes":{"caller":[{"name" .. if present & != "Unbekannt" etc.
+        caller_name = ''
+        if 'callerTypes' in obj:
+            if 'caller' in obj['callerTypes']:
+                for name_count in obj['callerTypes']['caller']:
+                    if name_count['name'] == 'Unbekannt':
+                        continue
+                    caller_name = name_count['name'] + ' '
+                    break
+        self.name = f'{caller_name}{self.location}'
+
+    def get_reverse_search_info(self):
+        """ Could do reverse search by DasOertliche by use of beautifulsoup4.
+        https://www.dasoertliche.de/Controller?form_name=search_inv&ph=07191952xxx """
+        pass
+
+
 class CallBlocker:
     """ Parse call monitor, examine RING event's phone number. """
 
-    def __init__(self, whitelist_pbids, blacklist_pbids, blocklist_pbid, min_score=6, min_comments=3, logger=None):
+    def __init__(self, whitelist_pbids, blacklist_pbids, blocklist_pbid,
+                 min_score=6, min_comments=3, logger=None):
         """ Provide a whitelist phonebook (normally first index 0) and where blocked numbers should go into. """
         self.whitelist_pbids = whitelist_pbids
         self.blacklist_pbids = blacklist_pbids
@@ -38,8 +77,8 @@ class CallBlocker:
         self.min_score = int(min_score)
         self.min_comments = int(min_comments)
         self.logger = logger
+        print("Retrieving data from Fritz!Box..")
         self.pb = Phonebook(address=FRITZ_IP_ADDRESS, user=FRITZ_USERNAME, password=FRITZ_PASSWORD)
-        self.onb_dict = dict()
         self.init_onb()
         self.set_area_and_country_code()
         for pb_id in self.whitelist_pbids + self.blacklist_pbids + [self.blocklist_pbid]:
@@ -69,6 +108,7 @@ class CallBlocker:
 
     def init_onb(self):
         """ Read the area codes into a dict. provided by BNetzA as CSV, separated by ';'. """
+        self.onb_dict = dict()
         with open('./data/onb.csv', encoding='utf-8') as csvfile:
             line_nr = 0
             csvreader = csv.reader(csvfile, delimiter=';')
@@ -95,9 +135,12 @@ class CallBlocker:
 
         if cm_line.type == CallMonitorType.RING.value:
 
+            # Simulate similar logging style to call monitor
+            dt = datetime.today().strftime('%d.%m.%y %H:%M:%S')
+
             number = cm_line.caller
             # Number can be with area_code or without! Both variants can be in phonebook, too..
-            number_variant = number  # Only used for search in whitelist
+            number_variant = number  # Only used for search in whitelist or blacklist
             if number.startswith(self.area_code):
                 number_variant = number.replace(self.area_code, '')  # Number wo AC
             if number.startswith('0'):
@@ -108,57 +151,54 @@ class CallBlocker:
 
             # 1. Is either full number 071..123... or short number 123... in the whitelist?
             if number in self.whitelist.keys() or number_variant in self.whitelist.keys():
-                log.debug(f'{number} in whitelist, skipping..')
+                log_str = f'{dt};WHITELISTED:{full_number};'
+                log.debug(log_str)
+                print(log_str)
                 if self.logger:
-                    self.logger(f'WHITELISTED:{number}')
+                    self.logger(log_str)
                 return
 
-            # 2. Already in blacklist?
-            if full_number in self.blacklist.keys():
-                log.warning(f'{number} in blacklist, skipping..')
+            # 2. Already in blacklist? Unsure, if short numbers can happen here, too?
+            if number in self.blacklist.keys() or number_variant in self.blacklist.keys():
+                log_str = f'{dt};BLACKLISTED:{full_number};'
+                log.debug(log_str)
+                print(log_str)
                 if self.logger:
-                    self.logger(f'BLACKLISTED:{number}')
+                    self.logger(log_str)
                 return
 
-            # 3. Get ratio for full_number, deserves maybe an extra method rate_number
-            # https://blog.tellows.de/2011/07/tellows-api-fur-die-integration-in-eigene-programme/
-            url = f'http://www.tellows.de/basic/num/{full_number}?json=1&partner=test&apikey=test123'
-            # ToDo: failed requests, blocked, json malformed..
-            req = requests.get(url)
-            obj = req.json()['tellows']
-            # print(obj)
-            score = int(obj['score'])
-            comment_count = int(obj['comments'])
-            location = obj['location']
-            searches = obj['searches']
-
-            # ToDo: instead of re-rate could cache ratings in a file? dict(full_number: object, ..)
+            # 3. Get ratio for full_number
+            ci = CallerInfo(full_number)
+            score_str = f'name:{ci.name};score:{ci.score};comments:{ci.comment_count};searches:{ci.searches};'
 
             # 4. Block if bad ratio
-            if score >= self.min_score and comment_count >= self.min_comments:
-                # ToDo: try to build a smarter name, e.g. add first callerTypes":{"caller":[{"name" .. if != Unbekannt
-                name = f'CallBlock {location} ({searches})'
+            if ci.score >= self.min_score and ci.comment_count >= self.min_comments:
+                name = "[CallBlocker] " + ci.name
                 result = self.pb.add_contact(self.blocklist_pbid, name, full_number)
-                if result:
+                if result:  # If not {} returned, it's an error
                     print(result)
-                # ToDo: log blocking
-                print(f'{full_number} BLOCKED')
-                if self.logger:
-                    self.logger(f'BLOCKED:{number};name:{name};score:{score};comments:{comment_count};')
-
-    # Ideas: reverse search via dasoertliche? Is there an API? Seems no, maybe a case for beautifulsoup
-    # https://www.dasoertliche.de/Controller?form_name=search_inv&ph=07191952xxx
+                action = "BLOCKED"
+            else:
+                action = "PASSED"
+            log_str = f'{dt};{action}:{full_number};{score_str}'
+            log.debug(log_str)
+            print(log_str)
+            if self.logger:
+                self.logger(log_str)
 
 
 if __name__ == "__main__":
     # Quick example how to use only
     cb_log = CallBlockerLog(daily=True, anonymize=False)
-    cb = CallBlocker(whitelist_pbids=[0], blacklist_pbids=[1,2], blocklist_pbid=2,
+    cb = CallBlocker(whitelist_pbids=[0], blacklist_pbids=[1, 2], blocklist_pbid=2,
                      min_score=6, min_comments=3, logger=cb_log.log_line)
-    cm_log = CallMonitorLog(file_prefix="callmonitor", daily=True, anonymize=False)
+    cm_log = CallMonitorLog(daily=True, anonymize=False)
     cm = CallMonitor(logger=cm_log.log_line, parser=cb.parse_and_examine_line)
 
-    # Provoke by test
+    # Provoke whitelist test
+    # test_line = '17.06.20 10:28:29;RING;0;07191952xxx;69xxx;SIP0;'
+    # cb.parse_and_examine_line(test_line)
+    # Provoke blacklist test
     # test_line = '17.06.20 10:28:29;RING;0;0781968053101;69xxx;SIP0;'
     # cb.parse_and_examine_line(test_line)
 
