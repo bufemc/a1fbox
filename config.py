2754f67cb7c825687fb89751a17b12b74be3d98d
bufemc <none>
2020-09-01 14:52:22 +0200

    Ignore and never upload/share personal configuration

--- config.py
+++
@@ -1,6 +0,0 @@
-# Config like in the documentation for fritzconnection, which is also used in this project
-FRITZ_IP_ADDRESS = 'fritz.box'
-FRITZ_TCP_PORT = 49000
-FRITZ_TLS_PORT = 49443
-FRITZ_USERNAME = 'dslf-config'
-FRITZ_PASSWORD = ''
